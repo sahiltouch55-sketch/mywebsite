fd6b2b977b6a492316a287ff8bc50284168c0c3c
sahiltouch55-sketch <sahiltouch55@gmail.com>
2026-03-15 15:50:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -100,6 +100,7 @@ let complaints = JSON.parse(localStorage.getItem("complaints")) || [];
   let complaintId = "C" + Math.floor(Math.random()*10000);
   let date = new Date().toLocaleString();
 complaints.push({
+  id:complaintld,
 name:name,
 text:complaint,
   status:"Pending",
